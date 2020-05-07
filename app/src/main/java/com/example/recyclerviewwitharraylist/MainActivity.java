e5e608b185ddf833bb158fa571aa994b15e52fe6
imran <imran@gmail.com>
2020-05-07 16:05:17 +0600

    adpater and viewholder marge in main activity

--- app/src/main/java/com/example/recyclerviewwitharraylist/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewwitharraylist/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.recyclerviewwitharraylist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> countryName=new ArrayList<>(Arrays.asList("Afghanistan","Bangladesh","China","Denmark","Ecuador","France","Greece","Hungary","India","Japan"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView= (RecyclerView)findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MyAdapter myAdapter =new MyAdapter(countryName,this);
+        recyclerView.setAdapter(myAdapter);
     }
 }
