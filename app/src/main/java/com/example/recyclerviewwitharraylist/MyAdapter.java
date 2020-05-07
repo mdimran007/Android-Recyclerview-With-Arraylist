93c597b5cf2ae14f3353c23dbc1d87ff5eff47cf
imran <imran@gmail.com>
2020-05-07 16:20:52 +0600

    set onclick method

--- app/src/main/java/com/example/recyclerviewwitharraylist/MyAdapter.java
+++ app/src/main/java/com/example/recyclerviewwitharraylist/MyAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,8 +30,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
         holder.countryName.setText(countryName.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(context,countryName.get(position),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
